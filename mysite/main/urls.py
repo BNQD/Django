d5ea12eefc51b4aa08c4461cc472492c5a2de413
Nam Doan <nam.quang.doan@gmail.com>
2020-04-30 16:10:28 -0400

    Add gitignore

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
-from . import views #From relative
-
-app_name = "main"
 
 urlpatterns = [
-    path("", views.homepage, name = "homepage"),
+    path('admin/', admin.site.urls),
 ]
