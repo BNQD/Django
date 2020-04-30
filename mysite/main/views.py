ba33b12881916057bcafabc7a91f4704fd427c10
Nam Doan <nam.quang.doan@gmail.com>
2020-04-30 15:54:31 -0400

    Admins and Apps - Django Tutorial - 7:10
    Added Models
    Added instance of model
    Created superuser account

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def homepage(request): #Any view - always pass request
+    return HttpResponse("This is an <strong>awesome</strong> tutorial")
